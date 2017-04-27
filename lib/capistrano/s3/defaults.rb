beffed672b6d00703381cd878de165fbe2749fd8
David Woods <drwoods21@gmail.com>
2017-04-27 11:42:52 -0500

    Default bucket write permission assumes aws v2 with fallback

--- lib/capistrano/s3/defaults.rb
+++ lib/capistrano/s3/defaults.rb
@@ -3,7 +3,7 @@ module Capistrano
     module Defaults
       DEFAULTS = {
         :deployment_path      => "public",
-        :bucket_write_options => { :acl => :public_read },
+        :bucket_write_options => { :acl =>  defined?(::Aws) ? :'public-read' : :public_read },
         :region               => 'us-east-1',
         :redirect_options     => {},
         :only_gzip            => false,
